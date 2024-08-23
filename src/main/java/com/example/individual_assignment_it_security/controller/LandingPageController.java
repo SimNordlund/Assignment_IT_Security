1e1666495aa39270b6347a7003710055838f098a
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-08-23 10:57:11 +0200

    Added landingpage etc.

--- src/main/java/com/example/individual_assignment_it_security/controller/LandingPageController.java
+++ src/main/java/com/example/individual_assignment_it_security/controller/LandingPageController.java
@@ -1,5 +1,6 @@
 package com.example.individual_assignment_it_security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,6 +8,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class LandingPageController {
 
     @GetMapping("/landing")
