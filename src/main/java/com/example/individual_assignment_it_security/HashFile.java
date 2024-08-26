b16e1cabf2040c930b6b2bc79c3d72d83ac80261
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-08-26 17:51:55 +0200

    Added MD5 class to hash words stored in password file and store them in hashes file. Works fine.

--- src/main/java/com/example/individual_assignment_it_security/HashFile.java
+++ src/main/java/com/example/individual_assignment_it_security/HashFile.java
@@ -1,17 +1,16 @@
 package com.example.individual_assignment_it_security;
 
-import org.codehaus.groovy.tools.shell.IO;
+import com.example.individual_assignment_it_security.crack.MD5;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HashFile implements CommandLineRunner {
+
+    MD5 md5;
+
     @Override
     public void run(String... args) throws IOException {
         System.out.println("HashFile fungerar");
@@ -28,11 +27,12 @@ public class HashFile implements CommandLineRunner {
             }
         }
 
-        System.out.println(passwordList);
-
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/com/example/individual_assignment_it_security/files/Hashes.txt"))) {
-            writer.write("bejs");
-            writer.newLine();  // To add a newline
+
+            for (int i = 0; i < passwordList.size(); i++) {
+                writer.write(passwordList.get(i) + ":" + MD5.getMd5(passwordList.get(i)));
+                writer.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
