ed84b2c50aa95cb6782519160f04b65e0ae4cb75
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-08-26 19:27:10 +0200

    Utils for hashing finished, can now hash both MD5 + SHA256

--- src/main/java/com/example/individual_assignment_it_security/HashFile.java
+++ src/main/java/com/example/individual_assignment_it_security/HashFile.java
@@ -1,6 +1,7 @@
 package com.example.individual_assignment_it_security;
 
 import com.example.individual_assignment_it_security.crack.MD5;
+import com.example.individual_assignment_it_security.crack.SHA256;
 import org.springframework.boot.CommandLineRunner;
 
 import java.io.*;
@@ -9,16 +10,13 @@ import java.util.List;
 
 public class HashFile implements CommandLineRunner {
 
-    MD5 md5;
-
     @Override
     public void run(String... args) throws IOException {
         System.out.println("HashFile fungerar");
 
+        //Läser in data ifrån Passwords.txt
         String tempLine;
         List<String> passwordList = new ArrayList<>();
-
-
         try (BufferedReader reader = new BufferedReader
                 (new FileReader("src/main/java/com/example/individual_assignment_it_security/files/Passwords.txt"))) {
 
@@ -27,11 +25,23 @@ public class HashFile implements CommandLineRunner {
             }
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/com/example/individual_assignment_it_security/files/Hashes.txt"))) {
+        //Hashar lösenord ifrån Passwords.txt med MD5. Sparar i HashesMD5.
+        try (BufferedWriter writerMD5 = new BufferedWriter(new FileWriter("src/main/java/com/example/individual_assignment_it_security/files/HashesMD5.txt"))) {
 
-            for (int i = 0; i < passwordList.size(); i++) {
-                writer.write(passwordList.get(i) + ":" + MD5.getMd5(passwordList.get(i)));
-                writer.newLine();
+            for (String s : passwordList) {
+                writerMD5.write(s + ":" + MD5.getMD5(s));
+                writerMD5.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Hashar lösenord ifrån Passwords.txt med SHA256. Sparar i HashesSHA256.
+        try (BufferedWriter writerSHA256 = new BufferedWriter(new FileWriter("src/main/java/com/example/individual_assignment_it_security/files/HashesSHA256.txt"))) {
+
+            for (String s : passwordList) {
+                writerSHA256.write(s + ":" + SHA256.getSHA256(s));
+                writerSHA256.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
