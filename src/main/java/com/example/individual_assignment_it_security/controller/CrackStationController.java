f10909008694a04476ce6af1be733f427b903f55
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-08-27 15:40:17 +0200

    Cracker works now.

--- src/main/java/com/example/individual_assignment_it_security/controller/CrackStationController.java
+++ src/main/java/com/example/individual_assignment_it_security/controller/CrackStationController.java
@@ -1,19 +1,44 @@
 package com.example.individual_assignment_it_security.controller;
 
+import com.example.individual_assignment_it_security.service.CrackStationServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.FileNotFoundException;
 
 @Controller
 @PreAuthorize("isAuthenticated()")
 public class CrackStationController {
 
+    private final CrackStationServiceImpl crackStationServiceImpl;
+
+    public CrackStationController (CrackStationServiceImpl crackStationServiceImpl) {
+        this.crackStationServiceImpl = crackStationServiceImpl;
+    }
+
+
     @GetMapping("/crack")
     public String crackStationPage(@AuthenticationPrincipal OAuth2User oAuth2User,
                                Model model) {
         return "crackstation";
+    }
+
+    @PostMapping("/crackHash")
+    public String crackHash(@RequestParam String hashToCrack, Model model) throws FileNotFoundException {
+
+        String crackedPasswordMD5 = crackStationServiceImpl.crackMD5(hashToCrack);
+        model.addAttribute("crackedPasswordMD5", crackedPasswordMD5);
+
+        String crackedPasswordSHA256 = crackStationServiceImpl.crackSHA256(hashToCrack);
+        model.addAttribute("crackedPasswordSHA256", crackedPasswordSHA256);
+
+        return "crackstationResult";
     }
 }
--- src/main/java/com/example/individual_assignment_it_security/service/CrackStationServiceImpl.java
+++ src/main/java/com/example/individual_assignment_it_security/service/CrackStationServiceImpl.java
@@ -1,4 +1,66 @@
 package com.example.individual_assignment_it_security.service;
 
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CrackStationServiceImpl {
+
+    public String crackMD5(String hashToCrack) {
+        String tempLine;
+        List<String> passwordList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader
+                (new FileReader("src/main/java/com/example/individual_assignment_it_security/files/HashesMD5.txt"))) {
+
+            while ((tempLine = reader.readLine()) != null) {
+                passwordList.add(tempLine);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String line : passwordList) {
+            int index = line.indexOf(':');
+            String password = line.substring(0, index);
+            String hash = line.substring(index + 1);
+
+            if (hashToCrack.equals(hash)) {
+                return password;
+            }
+        }
+        return "Not found";
+    }
+
+    public String crackSHA256(String hashToCrack) {
+        String tempLine;
+        List<String> passwordList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader
+                (new FileReader("src/main/java/com/example/individual_assignment_it_security/files/HashesSHA256.txt"))) {
+
+            while ((tempLine = reader.readLine()) != null) {
+                passwordList.add(tempLine);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String line : passwordList) {
+            int index = line.indexOf(':');
+            String password = line.substring(0, index);
+            String hash = line.substring(index + 1);
+
+            if (hashToCrack.equals(hash)) {
+                return password;
+            }
+        }
+        return "Not found";
+    }
 }
