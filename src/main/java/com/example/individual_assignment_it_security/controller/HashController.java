87490983a02f8febaac1c8f2590949a5d50d43de
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-08-27 11:35:46 +0200

    Attempting to get result of hashes on localhost.

--- src/main/java/com/example/individual_assignment_it_security/controller/HashController.java
+++ src/main/java/com/example/individual_assignment_it_security/controller/HashController.java
@@ -1,19 +1,36 @@
 package com.example.individual_assignment_it_security.controller;
 
+import com.example.individual_assignment_it_security.crack.MD5;
+import com.example.individual_assignment_it_security.crack.SHA256;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @PreAuthorize("isAuthenticated()")
 public class HashController {
 
     @GetMapping("/hash")
-    public String hashPage(@AuthenticationPrincipal OAuth2User oauth2User, Model model) {
+    public String hashPage(@AuthenticationPrincipal OAuth2User oauth2User,
+                           Model model) {
         return "hashpassword";
     }
+
+    @PostMapping("/createNewHash")
+    public String createNewHash(@RequestParam String password, Model model){
+        String hashedPasswordMD5 = MD5.getMD5(password);
+        String hashedPasswordSHA256 = SHA256.getSHA256(password);
+
+        model.addAttribute("hashedPasswordMD5", hashedPasswordMD5);
+        model.addAttribute("hashedPasswordSHA256", hashedPasswordSHA256);
+
+        return "hashpasswordResult";
+    }
+
 
 }
